0eb2214a9ecda951724b0a289016045bd5fd0e94
jayfeng0225 <jayfeng0225@gmail.com>
2016-09-30 00:50:31 +0800

    OpenMP ver

--- hw1/raytracing/math-toolkit.h
+++ hw1/raytracing/math-toolkit.h
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <assert.h>
+#include <omp.h>
 
 static inline
 void normalize(double *v)
@@ -29,18 +30,25 @@ void add_vector(const double *a, const double *b, double *out)
         out[i] = a[i] + b[i];
 }
 
-static inline
+static
 void subtract_vector(const double *a, const double *b, double *out)
 {
-    for (int i = 0; i < 3; i++)
-        out[i] = a[i] - b[i];
-}
+    //for (int i = 0; i < 3; i++)
+    //    out[i] = a[i] - b[i];
+	out[0] = a[0] - b[0];
+	out[1] = a[1] - b[1];
+	out[2] = a[2] - b[2];
+}__attribute__((always_inline))
 
 static inline
 void multiply_vectors(const double *a, const double *b, double *out)
 {
-    for (int i = 0; i < 3; i++)
-        out[i] = a[i] * b[i];
+    //for (int i = 0; i < 3; i++)
+    //    out[i] = a[i] * b[i];
+	out[0] = a[0] * b[0];
+	out[1] = a[1] * b[1];
+	out[2] = a[2] * b[2];
+
 }
 
 static inline
@@ -58,14 +66,16 @@ void cross_product(const double *v1, const double *v2, double *out)
     out[2] = v1[0] * v2[1] - v1[1] * v2[0];
 }
 
-static inline
+static
 double dot_product(const double *v1, const double *v2)
 {
     double dp = 0.0;
-    for (int i = 0; i < 3; i++)
-        dp += v1[i] * v2[i];
+ 	//#pragma omp parallel for num_threads(4)
+    //for (int i = 0; i < 3; i++)
+    //   dp += v1[i] * v2[i];
+	dp = v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2] ;
     return dp;
-}
+}__attribute__((always_inline))
 
 static inline
 void scalar_triple_product(const double *u, const double *v, const double *w,
